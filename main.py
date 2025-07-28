e19158cf53a34302bc1422240235213fc35f406b
Cankat <cankatfurtuna@outlook.com>
2025-07-28 16:50:57 +0300

    Update main.py
    Added error conditions and option to show answers

--- main.py
+++ main.py
@@ -3,17 +3,37 @@ def arithmetic_arranger(problems, show_answers=False):
     row1 = []
     row2 = []
     operator = []
+    problemCount = 0
+    numberCount = 0
     i = 0
     result = ""
-    #Assign row1, row2 and operator
+    answer = 0
+#Assign row1, row2, operator and handle errors
     for problem in problems:
+        problemCount +=1
         for char in problem.strip(" "):
+            
             if char == "+" or char == "-":
                 row1.append(row)
                 row = ""
                 operator += char
+                if numberCount > 4:
+                    return 'Error: Numbers cannot be more than four digits.'
+                numberCount = 0
             elif char.isdigit():
                 row += char
+                numberCount += 1
+            elif char == "/" or char =="*" or char =="x":
+                return "Error: Operator must be '+' or '-'."                
+            elif char ==" ":
+                pass
+            else:    
+                return 'Error: Numbers must only contain digits.'
+        if numberCount > 4:
+                    return 'Error: Numbers cannot be more than four digits.'
+        numberCount = 0
+        if problemCount > 5:
+            return 'Error: Too many problems.'
         row2.append(row)
         row = ""
 
@@ -48,7 +68,18 @@ def arithmetic_arranger(problems, show_answers=False):
             result += "-"
         result += "--    "
         i += 1
+    result += "\n"
 
+    #Fourth Row
+    i = 0
+    for op in operator:
+        if op == "+":
+            answer += int(row1[i]) + int(row2[i])
+        elif op == "-":
+            answer += int(row1[i]) - int(row2[i])
+        result +=  str(answer) + "       "
+        answer = 0
+        i += 1
     return result
 
 print(arithmetic_arranger(["39 + 8", "1 + 71","9999 + 9999", "523 - 49"]))
